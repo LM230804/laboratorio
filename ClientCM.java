5507392b8519553d39d0d8e47335f989bf8084fd
LM230804 <130073233+LM230804@users.noreply.github.com>
2024-08-22 18:31:36 +0200

    Update ClientCM.java

--- ClientCM.java
+++ ClientCM.java
@@ -8,75 +8,17 @@ import java.net.Socket;
 
 public class ClientCM {
    public static void main(String[] args) {
-      try {
-         Throwable var1 = null;
-         Object var2 = null;
+      try (Socket socket = new Socket("localhost", 5432);
+           PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+           BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-         try {
-            Socket socket = new Socket("localhost", 5432);
+         writer.println("registerUser johnDoe secretPassword");
+         String response = reader.readLine();
+         System.out.println("Response from server: " + response);
 
-            try {
-               PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-
-               try {
-                  BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                  try {
-                     writer.println("registerUser johnDoe secretPassword");
-                     String response = reader.readLine();
-                     System.out.println("Response from server: " + response);
-                  } finally {
-                     if (reader != null) {
-                        reader.close();
-                     }
-
-                  }
-               } catch (Throwable var28) {
-                  if (var1 == null) {
-                     var1 = var28;
-                  } else if (var1 != var28) {
-                     var1.addSuppressed(var28);
-                  }
-
-                  if (writer != null) {
-                     writer.close();
-                  }
-
-                  throw var1;
-               }
-
-               if (writer != null) {
-                  writer.close();
-               }
-            } catch (Throwable var29) {
-               if (var1 == null) {
-                  var1 = var29;
-               } else if (var1 != var29) {
-                  var1.addSuppressed(var29);
-               }
-
-               if (socket != null) {
-                  socket.close();
-               }
-
-               throw var1;
-            }
-
-            if (socket != null) {
-               socket.close();
-            }
-         } catch (Throwable var30) {
-            if (var1 == null) {
-               var1 = var30;
-            } else if (var1 != var30) {
-               var1.addSuppressed(var30);
-            }
-
-            throw var1;
-         }
-      } catch (IOException var31) {
-         var31.printStackTrace();
+      } catch (IOException e) {
+         e.printStackTrace();
       }
-
    }
 }
+
