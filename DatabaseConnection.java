05b0d9119f45a16a8157ee8dc6796cec5338cec4
LM230804 <130073233+LM230804@users.noreply.github.com>
2024-09-02 10:45:30 +0200

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class provaconn {
+public class DatabaseConnection() {
     public static void main(String[] args) {
         // URL del database, formato: jdbc:postgresql://host:port/database
         String url = "jdbc:postgresql://localhost:5432/MAINDB";
